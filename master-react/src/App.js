ae7fbff6673a18c584cae2fb1889b425e5304fc2
basucode <basu.code@gmail.com>
2023-07-23 15:50:31 +0530

    #1 - Created react app and instaleld tailwind.

---
+++ master-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="blank">
+        BEGIN
+      </div>
+    </div>
+  );
+}
+
+export default App;
