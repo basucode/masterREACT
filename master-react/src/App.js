19b6416de68c624ad60cc061d14c501a60677eb0
basucode <basu.code@gmail.com>
2023-07-23 16:23:48 +0530

    #2 - Made tailwind working.

--- master-react/src/App.js
+++ master-react/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="blank">
+      <div className="bg-neutral-800">
         BEGIN
       </div>
+      <p className="text-orange-500	text-7xl">BEGIN</p>
     </div>
   );
 }
